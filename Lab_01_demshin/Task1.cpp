6ec15f40e0de2a51ed14abbecd2150add2ce81c3
X-sicksu <demshin08@gmail.com>
2026-09-11 18:40:59 +0300

    11.09 patch

--- Lab_01_demshin/Task1.cpp
+++ Lab_01_demshin/Task1.cpp
@@ -1,18 +1,20 @@
 //Демшин
+//Ввод библиотек и начало функции
 #include <iostream>
 using namespace std;
 int main() {
 
-    setlocale(LC_ALL, "Russian");
+    setlocale(LC_ALL, "Russian");   //Ввод кирилицы в функцию
 
-double berry, sugar, water;
+double berry, sugar, water;         //Оглавление переменных
 
-cout << "Количество литров" << "-";
-cin >> water;
+cout << "Количество литров" << "-"; //Вывод в терминале фразы "Количество литров -"
+cin >> water;                       //Ввод количества воды 
 
-berry = water / 3;
-sugar = water / 2;
+berry = water / 3;                  //Инициализация переменной berry
+sugar = water / 2;                  //Инициализация переменной sugar
 
+//Вывод данных о количестве ингридиентов
 cout << "Ягод" << "-" << berry << "kg" << endl;
 cout << "Сахара" << "-" << sugar << "kg" << endl;
 cout << "Воды" << "-" << water << "li" << endl;
--- Lab_01_demshin/Task2.cpp
+++ Lab_01_demshin/Task2.cpp
@@ -1,24 +1,28 @@
 //Демшин
+//Ввод библиотек и начало функции
 #include <iostream>
 #include <cmath>
 using namespace std;
 int main(void) {
 
-    setlocale(LC_ALL, "Russian");
+    setlocale(LC_ALL, "Russian"); //Ввод кирилицы
 
+//Инициализация переменных
 int a = 3;
 int b = 4;
 int c = 5;
 
+//Вычисления
 int per = a + b + c;
 int pper = per / 2;
 
-int podroot = pper * (pper - a) * (pper - b) * (pper - c);
+int podroot = pper * (pper - a) * (pper - b) * (pper - c); //Вычисление формулы для подсчёта площади
 
-int plo = sqrt(podroot);
+int plo = sqrt(podroot); //Инициалиализация пременной отвечающей за площадь
 
+//Вывод данных о площади и периметре
 cout << "Периметр" << "-" << per << endl;
-cout << "Площадь"  << "-" <<plo << endl;
+cout << "Площадь"  << "-" << plo << endl;
      
     return 0;
 }
