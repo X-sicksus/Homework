0f97c454919fac0b84c2aef0f8d3e24815cac16a
X-sicksu <demshin08@gmail.com>
2026-09-15 12:00:06 +0300

    15.09.1 patch

---
+++ Lab_02_demshin/Task1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main();
+{
+    setlocale(LC_ALL, "Russian");
+
+    int a;
+    if(a=6; a<=23);
+        else();
+
+
+
+
+
+
+
+
+}
---
+++ Lab_02_demshin/Task3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main();
+{
+    setlocale(LC_ALL, "Russian");
+
+    int Месяц;
+    cout >> ("Введите номер месяца (1-12)")
+    cin << Месяц;
+
+    switch
+
+
+
+
+}
